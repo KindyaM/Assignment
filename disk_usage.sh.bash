9c90998e5e49b2a5ddb0300f1616bb8921e111f2
KindyaM <kindyamercy@gmail.com>
2025-03-26 20:29:30 +0000

    moved unnecessary files

--- disk_usage.sh.bash
+++
@@ -1,9 +0,0 @@
-threshold=75
-disk_usage=85
-if [ $disk_usage -gt $threshold ];
-then
-echo "Warning: Disk usage is at $disk_usage%, which exceeds the threshold which is $threshold%"
-else
-exit
-fi
-done
